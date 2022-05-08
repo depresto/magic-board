ba6af1e1a1ec5f09b43cc5abb26fdec135a5afd7
Wayne Huang <wayne123tw@gmail.com>
2022-05-08 17:30:29 +0800

    [FEATURE] new number line

--- src/components/widget/NumberLine.tsx
+++ src/components/widget/NumberLine.tsx
@@ -29,7 +29,9 @@ const NumberLine: React.FC<NumberLineProps> = ({
   intervalEnd = defaultIntervalEnd,
   baseDominator = defaultBaseDominator,
 }) => {
+  const [numberLineWidth, setNumberLineWidth] = useState(400);
   const [lineGroup, setLineGroup] = useState<fabric.Group | null>(null);
+  const [arrowObject, setArrowObject] = useState<fabric.Group | null>(null);
   const [imgRef, setImgRef] = useState<HTMLImageElement | null>(null);
   const [imgSrc, setImgSrc] = useState("");
 
@@ -49,12 +51,12 @@ const NumberLine: React.FC<NumberLineProps> = ({
     const lineStrokeColor = "black";
     const lineStrokeLinCap = "round";
     const lineStrokeWidth = 2;
+    const arrowStrokeWidth = 3;
     const fontSize = 28;
     const fontFamily = "san-serif";
 
     const numberLineStartX = 0;
     const numberLineStartY = 0;
-    const numberLineWidth = 400;
     const numberLineHeight = 36;
     const numberLineGapHeight = 20;
     const arrowWidth = 20;
@@ -110,22 +112,23 @@ const NumberLine: React.FC<NumberLineProps> = ({
 
     const arrowUpper = new fabric.Line([0, 0, arrowLength, arrowLength], {
       stroke: lineStrokeColor,
-      strokeWidth: lineStrokeWidth,
+      strokeWidth: arrowStrokeWidth,
       strokeLineCap: lineStrokeLinCap,
       left: numberLineStartX + numberLineWidth + arrowWidth,
       top: numberLineStartY + numberLineHeight / 2 - arrowLength,
     });
     const arrowLower = new fabric.Line([0, arrowLength, arrowLength, 0], {
       stroke: lineStrokeColor,
-      strokeWidth: lineStrokeWidth,
+      strokeWidth: arrowStrokeWidth,
       strokeLineCap: lineStrokeLinCap,
       left: numberLineStartX + numberLineWidth + arrowWidth,
       top: numberLineStartY + numberLineHeight / 2,
     });
-    const arrowObject = new fabric.Group([arrowUpper, arrowLower]);
-    arrowObject.on('mouse:over', e => {
-      console.log(e)
-    })
+    const arrowObject = new fabric.Group([arrowUpper, arrowLower], {
+      perPixelTargetFind: true,
+      hoverCursor: "pointer",
+    });
+    setArrowObject(arrowObject);
     lineGroupObjects.push(arrowObject);
 
     counter = 0;
@@ -150,6 +153,7 @@ const NumberLine: React.FC<NumberLineProps> = ({
       left: initialX || 0,
       top: initialY || 0,
       angle: initialAngle || 0,
+      subTargetCheck: true,
     });
     setLineGroup(lineGroup);
   }, [
@@ -160,7 +164,65 @@ const NumberLine: React.FC<NumberLineProps> = ({
     initialY,
     intervalEnd,
     intervalStart,
+    numberLineWidth,
   ]);
+
+  const [isArrowDragging, setIsArrowDragging] = useState(false);
+  useEffect(() => {
+    const onArrowMouseOver = (e: fabric.IEvent<Event>) => {
+      const objects: fabric.Line[] = (e.target as any)._objects;
+      for (const object of objects) {
+        object.set("stroke", "red");
+      }
+      e.target?.canvas?.requestRenderAll();
+    };
+    const onArrowMouseOut = (e: fabric.IEvent<Event>) => {
+      const objects: fabric.Line[] = (e.target as any)._objects;
+      for (const object of objects) {
+        object.set("stroke", "black");
+      }
+      e.target?.canvas?.requestRenderAll();
+    };
+    const onArrowMouseDown = () => {
+      setIsArrowDragging(true);
+    };
+    const onArrowMouseUp = () => {
+      setIsArrowDragging(false);
+    };
+
+    arrowObject?.on("mouseover", onArrowMouseOver);
+    arrowObject?.on("mouseout", onArrowMouseOut);
+    arrowObject?.on("mousedown", onArrowMouseDown);
+    arrowObject?.on("mouseup", onArrowMouseUp);
+    return () => {
+      arrowObject?.off("mouseover", onArrowMouseOver);
+      arrowObject?.off("mouseout", onArrowMouseOut);
+      arrowObject?.off("mousedown", onArrowMouseDown);
+      arrowObject?.off("mouseup", onArrowMouseUp);
+    };
+  }, [arrowObject]);
+
+  useEffect(() => {
+    const onArrowMouseMove = (e: fabric.IEvent<Event>) => {
+      if (e.target && isArrowDragging) {
+        const rightPosX = (e.target.left || 0) + (e.target.width || 0);
+        const objectWidthDelta = (e.absolutePointer?.x || 0) - rightPosX;
+        setNumberLineWidth(
+          (numberLineWidth) => numberLineWidth + objectWidthDelta
+        );
+      }
+
+      if (lineGroup) {
+        lineGroup.lockMovementX = isArrowDragging;
+        lineGroup.lockMovementY = isArrowDragging;
+      }
+    };
+
+    arrowObject?.on("mousemove", onArrowMouseMove);
+    return () => {
+      arrowObject?.off("mousemove", onArrowMouseMove);
+    };
+  }, [arrowObject, isArrowDragging, lineGroup]);
 
   useEffect(() => {
     if (lineGroup && imgRef) {
